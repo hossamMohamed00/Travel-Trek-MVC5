8652c693f8de0f81d4c34bf07fcf018e1886ee77
Hossam Mohamed <61068354+hossamMohamed00@users.noreply.github.com>
2021-05-29 18:36:35 +0200

    Add the ability to search (But need enhance later)

---
+++ Travel-Trek/Scripts/Custom/SearchForPost.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function() {
+    /* This function to compare values */
+    function Contains(text_one, text_two) {
+        if (text_one.indexOf(text_two) != -1)
+            return true;
+    }
+
+    /* Get the search term and apply the filter */
+    $("#Search").keyup(function() {
+        var searchTerm = $("#Search").val().toLowerCase();
+        /* Loop  all the posts and filter it */
+        $(".posts").each(function() {
+            if (!Contains($(this).text().toLowerCase(), searchTerm)) {
+                $(this).hide();
+            } else {
+                $(this).show();
+            }
+        });
+    });
+})
